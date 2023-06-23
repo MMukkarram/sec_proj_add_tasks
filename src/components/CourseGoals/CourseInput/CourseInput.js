dfefe7e819b02fdd3efd2cd44957566c7593252e
Muhammad Mukkarram <mukkarram.moin@venturedive.com>
2023-06-23 13:56:08 +0500

    Init

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,27 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
+    if (enteredValue.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label /* style={{ color: !isValid ? "red" : "black" }}  */ >Course Goal</label>
+        <input
+          type="text"
+          /*style={{
+            borderColor: !isValid ? "red" : "none",
+            background: !isValid ? "salmon" : "none",
+          }}*/
+          onChange={goalInputChangeHandler}
+        />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button
+        type="submit"
+       // style={{ backgroundColor: !isValid ? "red" : "none" }}
+      >
+        Add Goal
+      </Button>
     </form>
   );
 };
